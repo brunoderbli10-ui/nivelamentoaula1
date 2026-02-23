5e6371462e7dd50eb0c8a0111ee71c1dd030b0ea
brunoderbli10-ui <brunoderbli10@gmail.com>
2026-02-23 11:31:00 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,29 @@
+### Atividade 1 - Nivelamento ###
+# Objetivo: Criar sua autodescrição utilizando variáveis e função print().
+# 1 - Pesquise o que é autodescrição e para quê ela serve.
+
+# 2 - Crie as seguintes variáveis:
+nome = "Bruno"
+idade = 16
+altura = 1.61
+genero = "Masculino"
+cor_de_pele = "Branca"
+cor_de_cabelo = "loiro escuro"
+tamanho_cabelo = "curto"
+estilo_cabelo = "enrolado"
+# 3 - Crie 2 variáveis: uma armazena o curso que você faz, a outra armazena o seu hobby favorito.
+hobby = "tirar fotos e caminhar"
+curso = "Desenvolvimento de Sistemas"
+
+# 4 - Utilizando a função print(), mostre o texto completo da sua autodescrição, utilizando as variáveis que você criou!
+# Exemplo: "Olá, meu nome é Fulano, tenho 999 anos e 1.90 de altura... (continue até usar todas as variáveis)"
+print (f" Olá, meu nome é {nome} e sou do gênero {genero}, tenho {idade} anos e {altura} de altura. \n Minha cor de pele é {cor_de_pele}, e tenho o cabelo {cor_de_cabelo} enquanto, ele é {tamanho_cabelo} e {estilo_cabelo}. \n Meu curso é {curso} e meus hobbys são {hobby}.")
+
+## Forma de avaliação: ##
+# 0,5 ponto = Preenchimento correto das variáveis do item 2
+# 0,5 ponto = Criação das variáveis do item 3
+# 1,0 ponto = Coerência do texto e uso correto das variáveis na função print()
+# TOTAL: 2,0 pontos
+
+## Entrega: ##
+# O aluno deve criar um novo repositório no GitHub, com o nome "1TRI-DesenvolvimentoSistemas" e realizar upload do arquivo .py.
